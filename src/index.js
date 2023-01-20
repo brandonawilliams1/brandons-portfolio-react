b2ed351418dfdea9e4200e6b41bb41b4fa91a2b2
brandon <brandonawilliams1@gmail.com>
2023-01-20 02:38:00 -0600

    defined path in routes

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import App from './App';
 import Contact from './components/Contact';
-import Works from './components/Works'
+import Works from './components/Works';
+
+
 
 ReactDOM.render(
 
   <Router>
     <Routes>
-      {/* changed the path from / to my repo end path */}
-      <Route path='/brandon-profile' element={<App/>}/>
+      <Route path='/brandons-portfolio-react' element={<App/>}/>
       <Route path='/contact' element={<Contact/>}/>
       <Route path='/works' element={<Works/>}/>
     </Routes>
